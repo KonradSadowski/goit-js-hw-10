c77fdc0d6e4837ceee4a6baf3bbd2c50f6d75e35
KonradSadowski <darnokowsky@gmail.com>
2023-04-22 16:29:29 +0200

    zrobione

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,13 @@
+import Notiflix, { Notify } from 'notiflix';
 const API_URL = `https://restcountries.com/v3.1/name`;
 
 export const fetchCountries = name => {
   return fetch(
     `${API_URL}/${name}?fields=name,capital,population,flags,languages`
-  ).then(res => res.json());
+  ).then(response => {
+    if (!response.ok) {
+      Notify.failure('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,57 +1,40 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix, { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
-
 const countryListEl = document.querySelector(`.country-list`);
 const countryInfoEl = document.querySelector(`.country-info`);
 const searchBoxEl = document.querySelector(`#search-box`);
-searchBoxEl.addEventListener('input', event => {
-  console.log(searchBoxEl.value);
-});
 
-// MOJE PRÓBY
-// const searchBoxEl = document.querySelector(`#search-box`);
-// const countryListEl = document.querySelector(`.country-list`);
-// const countryInfoEl = document.querySelector(`.country-info`);
+searchBoxEl.addEventListener(
+  'input',
+  _.debounce(async event => {
+    const countryName = event.target.value.trim();
 
-// let name = searchBoxEl.value;
-// const API_URL_NAME = `https://restcountries.com/v3.1/name/${name}`;
-// const API_URL_ALL = `https://restcountries.com/v3.1/all`;
+    if (countryName === '') {
+      countryInfoEl.innerHTML = '';
+      countryListEl.innerHTML = '';
+      return;
+    }
+    const countries = await fetchCountries(countryName);
 
-// const fetchUsersBtn = document.querySelector('.btn');
+    if (countries.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    } else {
+      countryListEl.innerHTML = countries
+        .map(
+          country =>
+            `<li><img class="flag" src="${country.flags.png}">${country.name.common.toUpperCase()}</li>`
+        )
+        .join('');
+    }
 
-// fetchUsersBtn.addEventListener('click', () => {
-//   fetchCountries()
-//     .then(API_URL_ALL => renderCountryList(API_URL_ALL))
-//     .catch(error => console.log(error));
-// });
-
-// searchBoxEl.addEventListener('input', event => {
-//   console.log(searchBoxEl.value);
-// });
-
-// function fetchCountries(name) {
-//   return fetch(API_URL_ALL).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
-// function renderCountryList(name) {
-//   const markup = name
-//     .map(country => {
-//       return `
-//             <li>
-//               <p><b>Name</b>: ${country.name.official}</p>
-//               <p><b>Capital</b>: ${country.capital}</p>
-//               <p><b>Population</b>: ${country.population}</p>
-//               <p><b>Languages</b>: ${country.languages}</p>
-//             </li>
-//         `;
-//     })
-//     .join('');
-//   countryListEl.innerHTML = markup;
-// }
+    if (countries.length === 1) {
+      countryInfoEl.innerHTML = `
+      <p>Capital: ${countries[0].capital}</p>
+      <p>Population: ${countries[0].population}</p>
+      <p>Langueages: ${Object.values(countries[0].languages).join(', ')}</p>`;
+    }
+  }, DEBOUNCE_DELAY)
+);
